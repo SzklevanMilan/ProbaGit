b7572cceb54cb534806bce19a7a7cd902150ebb3
SzklevanMilan <szklevanm@gmail.com>
2020-09-07 11:59:50 +0200

    First line

--- ProbaGit/Program.cs
+++ ProbaGit/Program.cs
@@ -10,7 +10,9 @@ namespace ProbaGit
     {
         static void Main(string[] args)
         {
-          
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
         }
     }
 }
